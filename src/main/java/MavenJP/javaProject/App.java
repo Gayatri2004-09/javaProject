25060842ece81332f809e26f69623343e4acdd67
Gayatri <gayatridevi@gmail.com>
2024-11-14 19:48:07 +0530

    First commit to maven java project

---
+++ src/main/java/MavenJP/javaProject/App.java
@@ -0,0 +1,16 @@
+package MavenJP.javaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "K.Gayatri Devi" );
+        System.out.println( "22BD1A6651" );
+        
+    }
+}
